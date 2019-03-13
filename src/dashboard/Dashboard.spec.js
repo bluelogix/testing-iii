5131aaf36e26cfe200026e729ee8bdfe40b47bb5
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2019-03-13 17:23:42 -0400

    added testing to dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,2 +1,23 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
+import Dashboard from  './Dashboard';
+
+
+describe('<Dashboard />', () => {
+
+      it('should display controls and display components', () => {
+        const { getByText } = render(<Dashboard />);
+  
+        const unlocked = getByText(/unlocked/i);
+        const open = getByText(/open/i);
+        const lockGate = getByText(/lock gate/i);
+        const closeGate = getByText(/close gate/i);
+
+        expect(getByText(/unlocked/i)).toBeInTheDocument;
+        expect(getByText(/open/i)).toBeInTheDocument;
+        expect(getByText(/lock gate/i)).toBeInTheDocument;
+        expect(getByText(/close gate/i)).toBeInTheDocument;
+       
+      });
+  
+});
